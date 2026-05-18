84ada207a41a2888b95890052137d604579dad50
Luis-Gonzo-Velez <gadiel2311@outlook.com>
2026-05-18 10:28:08 -0400

    Chapter 52

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(202)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(202)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
-});
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+const tours = fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`);
+app.get('/api/v1/tours', (req, res) => {});
 
 const port = 3000;
 app.listen(port, () => {
