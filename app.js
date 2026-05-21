4f535ba47b34dba68bb2ebdc3014fdb33f47e26f
Luis-Gonzo-Velez <gadiel2311@outlook.com>
2026-05-21 12:34:03 -0400

    Chhapter 55

--- app.js
+++ app.js
@@ -72,6 +72,13 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  res.status(200).json({
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
