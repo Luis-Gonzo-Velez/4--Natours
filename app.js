2d5baf447cd59dbc779bd304f1fc3c47b654d9d1
Luis-Gonzo-Velez <gadiel2311@outlook.com>
2026-06-09 13:43:05 -0400

    create folder and organize postman

--- app.js
+++ app.js
@@ -107,17 +107,43 @@ const deleteTour = (req, res) => {
   });
 };
 
-const getAllUsers = (req, res) => {};
-
-// app.get('/api/v1/tours', getAllTours);
-// app.post('/api/v1/tours', createTour);
-// app.get('/api/v1/tours/:id', getTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
 
 //3) ROUTES
 
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app
+.route('/api/v1/tours')
+.get(getAllTours)
+.post(createTour);
 
 app
   .route('/api/v1/tours/:id')
@@ -125,7 +151,10 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
-app.route('/api/v1/users').get(getAllUsers).post(createuser);
+app
+.route('/api/v1/users')
+.get(getAllUsers)
+.post(createUser);
 
 app
   .route('/api/v1/users/:id')
