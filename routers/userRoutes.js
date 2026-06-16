ff5f01eea4e685a9e1f734ef96c333b738cf204c
Luis-Gonzo-Velez <gadiel2311@outlook.com>
2026-06-16 12:25:37 -0400

    fixing codes

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -1,9 +1,44 @@
-const userRouter = express.Router();
+const express = require('express');
 
-userRouter.route('/api/v1/users').get(getAllUsers).post(createUser);
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
 
-userRouter
+const router = express.Router();
+
+router.route('/api/v1/users').get(getAllUsers).post(createUser);
+
+router
   .route('/api/v1/users/:id')
   .get(getUser)
   .patch(updateUser)
   .delete(deleteUser);
+
+module.exports = router;
